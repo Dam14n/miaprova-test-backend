8963f4255417af97c29d164ad1f56304ecd1f25c
JulianSanse <juliansanse4@gmail.com>
2022-01-14 08:05:32 -0300

    #35

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,8 @@ let storage = multer.diskStorage({
     filename: (req, file, cb) => {
         cb(null, req.query.route + path.extname(file.originalname));
     }
-})
+});
+
 const upload = multer({ storage: storage });
 
 router.use(cors({ origin: whiteList }));
@@ -83,6 +84,32 @@ router.get("/api/files", (req, res) => {
         }
         console.log(files);
         res.send(files);
+    });
+});
+
+router.get("/api/fileParty", (req, res) => {
+    let fileName = req.query.name;
+    console.log(fileName);
+
+    let pointIndex = fileName.lastIndexOf(".");
+    let name = fileName.substring(0, pointIndex);
+    let ext = fileName.substring(pointIndex);
+
+    console.log("File name= " + name);
+    console.log("File Extension= " + ext);
+
+    let file = "";
+    if (name == "first-party ID") {
+        file = "batch-first-party-id_download";
+    } else if (name == "second-party ID") {
+        file = "batch-second-party_download";
+    } else {
+        file = ""
+    }
+    console.log("file = " + file);
+    const $file = `${__dirname}/../files/${file}.txt`;
+    res.download($file, `${__dirname}/../files/${name}${ext}`, () => {
+        console.log("\nFile Renamed!\n")
     });
 });
 
